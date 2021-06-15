0db36ffb52cdfc92ace254c4979932129a758e02
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-06-15 19:58:23 -0400

    copied some of comment model to post model

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,49 @@
 // THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
+// Copied some of comment js model and refactored
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Post extends Model {}
+
+  Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    postTitle: {
+      type: DataTypes.STRING(25),
+      allowNull: false,
+      unique: true,
+    },
+    postContent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    dateCreated: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },  
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
