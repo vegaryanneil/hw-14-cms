fe5e146ec190e61f5eaefecf930a03de36bc6b40
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-06-15 20:17:03 -0400

    Modified line 16 in dashboard-routes js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,12 +5,15 @@ const withAuth = require('../utils/auth');
 router.get('/', withAuth, async (req, res) => {
   try {
     // store the results of the db query in a variable called postData. should use something that "finds all" from the Post model. may need a where clause!
-
+    const postData = await Post.findAll({
+      where:{'userId': req.session.userId},
+      include: [User]
+    })
     // this sanitizes the data we just got from the db above (you have to create the above)
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // fill in the view to be rendered
-    res.render('all-posts-admin', {
+    res.render('all-posts', {
       // this is how we specify a different layout other than main! no change needed
       layout: 'dashboard',
       // coming from line 10 above, no change needed
