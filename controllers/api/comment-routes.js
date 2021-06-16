d1a6606f81d9a606e817b389de0a3876e2955882
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-06-15 20:27:13 -0400

    Added route to homepage after single post in comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,7 +2,20 @@ const router = require('express').Router();
 const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.findAll ({
+      include: [User],
+    });
+    const comments = commentData.map((comment) => comment.get({ plain:true}));
+    res.render('single-post', {comments, loggedIn: req.session.loggedIn});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post('/', withAuth, async (req, res) => {
+  const body = req.body;
   try {
     const newComment = await Comment.create({
       ...req.body,
